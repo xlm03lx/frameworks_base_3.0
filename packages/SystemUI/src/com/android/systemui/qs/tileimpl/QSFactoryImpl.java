593afce5289a8470fa3378821bc69a0aaf3e4267
SpiritCroc <spiritcroc@gmail.com>
2017-09-28 14:36:02 +0200

    QS: Locale Tile
    Squashed: required parts of:
    
        Author: SpiritCroc <dev@spiritcroc.de>
        Date:   Thu Nov 29 08:00:02 2018 +0100
    
        QS language/locale tile: Update drawable handling to Pie
    
        Change-Id: Ib929729f11fefc8e1891b4b7b3cf6d8a16d6555e
    
        Author: SpiritCroc <spiritcroc@gmail.com>
        Date:   Thu Sep 28 14:36:02 2017 +0200
    
        QS: Locale Tile
    
        Toggle the primary default language
    
        - Bring it to Oreo - @LorDClockaN
        - Bring it to Pie - @eyosen
    
        Change-Id: I3e336eddb360ea1a796727657652ad9301007380
    
    Change-Id: I00fbb0d124f6bd4bb9caa471c476581ec9aff97f

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -45,6 +45,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LiveDisplayTile;
+import com.android.systemui.qs.tiles.LocaleTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NfcTile;
@@ -169,6 +170,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new SoundTile(mHost);
             case "lte":
                 return new LteTile(mHost);
+            case "locale":
+                return new LocaleTile(mHost);
         }
 
         // Intent tiles.
