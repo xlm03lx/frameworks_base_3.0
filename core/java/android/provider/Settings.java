e1454664ae5f6bef571ad84251244fa07453949a
koron393 <koron393@gmail.com>
2019-05-17 01:03:07 +0900

    [1/2] FWB: Add back OmniSwitch Recent
    Squashed: required parts of:
        Author: maxwen <max.weninger@gmail.com>
        Date:   Tue Aug 14 15:03:51 2018 +0200
    
        [1/2] base: OmniSwitch integration
    
        do it on a lower level this time so we catch all
        calls in one place
    
        Change-Id: Ic603d0dde9fcf1f266b1bff2eb89dae5345168b7
    
    Change-Id: Id2ec17e9de3450b57abeb898eff5a9dfc7f8db50

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5003,6 +5003,12 @@ public final class Settings {
         public static final String GAMING_MODE_DYNAMIC_STATE = "gaming_mode_dynamic_state";
 
         /**
+         * Whether to enable OmniSwitch as recents implementation.
+         * @hide
+         */
+        public static final String RECENTS_OMNI_SWITCH_ENABLED = "recents_omni_switch";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5178,6 +5184,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(EGG_MODE);
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
+            PRIVATE_SETTINGS.add(RECENTS_OMNI_SWITCH_ENABLED);
         }
 
         /**
